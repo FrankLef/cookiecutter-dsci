8dd00f7ae456e5be96c8af9c0048ef762fe70f82
FrankLef <flefebvre01@hotmail.com>
2022-10-09 15:05:12 -0400

    init

--- {{cookiecutter.project_name}}/config/__init__.py
+++ {{cookiecutter.project_name}}/config/__init__.py
@@ -2,7 +2,7 @@
 
 Hydra is a package that maintains the configuraiton settings of projects.
 The official documentaiton does not show that an __init__.py file required
-but the author in a recent blog (I donèt have the url) says that it is 
+but the author in a recent blog (I donèt have the url) says that it is
 required for some functions. I noticed Hydra is about three times faster
 when there is an __init__.py file.
 """
